953591b289b623b7227b2f571be7d2cb85917cb2
Mathias Kriebel <mathias.kriebel@gmail.com>
2025-02-15 21:37:30 -0300

    inital commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Webhook Tester Frontend
+
+Simple tool to create HTTP endpoints and analyze the received requests.
+
+## Running
+```sh
+npm run dev
+```
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-9xl text-amber-700">Webhook Tester</h1>
+      <p className="text-3xl text-teal-800">
+        Hello world
+      </p>
+    </>
+  )
+}
+
+export default App
